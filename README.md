531c498bf4861cbac2c77953ae218ae60eff5ad6
franco401 <francojimenez168@gmail.com>
2024-06-18 22:23:04 -0400

    Added login registration and hashing functions to backend

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 A full stack, email application using Java in the backend with Spring Boot and JavaScript in the frontend with React.
 
 # Versions Used
-* Spring Boot: 3.3.0
-* Java: 22
+* Spring Boot 3.3.0
+* Java 22
 
 # Spring Boot Dependencies
 * Lombok
 * Spring Web
 * PostgreSQL Driver
+* Apache Commons Codec 1.17
 
 # React Dependencies
 ``` npm install react ```
--- emailapp/src/main/java/com/example/emailapp/Controllers/UserController.java
+++ emailapp/src/main/java/com/example/emailapp/Controllers/UserController.java
@@ -16,13 +16,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 //used to create api endpoints for controller
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.ResponseEntity;
 //used to allow different request origins
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+//used for password hashing
+import org.apache.commons.codec.digest.DigestUtils;
 
 //used to execute db queries
-import java.sql.PreparedStatement;
 import java.sql.*;
 import java.util.ArrayList;
 
@@ -38,6 +39,8 @@ public class UserController {
         //try-with-resources automatically closes the ps variable
         try (PreparedStatement ps = conn.prepareStatement(query)) {
             ResultSet rs = ps.executeQuery();
+
+            //read returned data from query in result set
             while (rs.next()) {
                 /*
                  * create a user object for each row returned from 
@@ -60,10 +63,11 @@ public class UserController {
         Connection conn = Database.connect();
         String query = "insert into \"Users\" values (?, ?, ?)";
         String id = Security.createBase64ID(userForm.email);
-        User user = new User(id, userForm.email, userForm.password);
+        User user = new User(id, userForm.email, DigestUtils.sha256Hex(userForm.password));
 
         //try-with-resources automatically closes the ps variable
         try (PreparedStatement ps = conn.prepareStatement(query)) {
+            //set query parameters and then execute
             ps.setString(1, id);
             ps.setString(2, user.email());
             ps.setString(3, user.password());
@@ -75,5 +79,41 @@ public class UserController {
             System.out.println("Query error: " + e);
         }
         return user;
+    }
+
+    @PostMapping("/login")
+    public User login(@RequestBody UserForm userForm) {
+        //connect to database and read post request data mapped into userForm
+        Connection conn = Database.connect();
+        String query = "select id, email from \"Users\" where email = ? and password = ?";
+        
+        //store data returned from database query for returning User object below
+        String id = "";
+        String email = "";
+
+        //try-with-resources automatically closes the ps variable
+        try (PreparedStatement ps = conn.prepareStatement(query)) {
+            //set query parameters and then execute
+            ps.setString(1, userForm.email);
+            ps.setString(2, DigestUtils.sha256Hex(userForm.password));
+            ResultSet rs = ps.executeQuery();
+            
+            //read returned data from query in result set
+            while (rs.next()) {
+                id = rs.getString("id"); 
+                email = rs.getString("email"); 
+            }
+
+            //close connection once finished with db query
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Query error: " + e);
+        }
+        /*
+         * create a user object for the row in
+         * the database that matches the entered
+         * username and password
+         */
+        return new User(id, email, "");
     }
 }
